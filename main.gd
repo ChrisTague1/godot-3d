61a601a0e758749aaa83aa6fd4681836353eeef2
Chris Tague <cstague6401@gmail.com>
2025-09-07 14:51:45 -0500

    stuff

---
+++ main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	
+	mob_spawn_location.progress_ratio = randf()
+	
+	var player_position = $Player.position # could I not use this syntax above instead of the weird get_node function?
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	add_child(mob)
