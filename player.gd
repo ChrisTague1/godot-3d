eb7b3ac513d31dbfb50f961eac0895e9ea38e382
Chris Tague <cstague6401@gmail.com>
2025-09-07 22:00:37 -0500

    more stuff

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var jump_impluse = 20
 
 var target_velocity = Vector3.ZERO
 
@@ -26,6 +27,9 @@ func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impluse
 	
 	velocity = target_velocity
 	move_and_slide()
